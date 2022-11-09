37018150b85153ff44c89a2f38d5b058c58888db
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-11-09 11:25:10 +0300

    more changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,10 @@ class Book < ActiveRecord::Base
     has_many :users, through: :reviews
 	belongs_to :genre
     
+    validates :title, presence: true
+    validates :description, presence: true
+
+
 	def self.search(search)
 		if search
 			genre = Genre.find_by(title: search)
@@ -11,5 +15,13 @@ class Book < ActiveRecord::Base
 			Book.all
 		end
 	end
+  
 
+    def average_rating
+        reviews = BookReview.where(["book_id = ?", id])
+        count = 0.0
+        reviews.each { |x| 
+        count += x.rating.to_f }
+        puts "Average Rating across #{reviews.size} reviews: " + "#{(count/reviews.size).round(2)}"
+      end
 end
