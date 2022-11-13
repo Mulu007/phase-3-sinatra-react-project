5b847af1b1491f79223347dd98e6b4a1eee5c395
Mulu <brianmulu007@gmail.com>
2022-11-13 20:54:24 +0300

    postgres

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,27 +1,27 @@
 class Book < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
-	belongs_to :genre
+	# belongs_to :genre
     
-    validates :title, presence: true
-    validates :description, presence: true
+    # validates :title, presence: true
+    # validates :description, presence: true
 
 
-	def self.search(search)
-		if search
-			genre = Genre.find_by(title: search)
+	# def self.search(search)
+	# 	if search
+	# 		genre = Genre.find_by(title: search)
 
-		else
-			Book.all
-		end
-	end
+	# 	else
+	# 		Book.all
+	# 	end
+	# end
   
 
-    def average_rating
-        reviews = BookReview.where(["book_id = ?", id])
-        count = 0.0
-        reviews.each { |x| 
-        count += x.rating.to_f }
-        puts "Average Rating across #{reviews.size} reviews: " + "#{(count/reviews.size).round(2)}"
-      end
+    # def average_rating
+    #     reviews = BookReview.where(["book_id = ?", id])
+    #     count = 0.0
+    #     reviews.each { |x| 
+    #     count += x.rating.to_f }
+    #     puts "Average Rating across #{reviews.size} reviews: " + "#{(count/reviews.size).round(2)}"
+    #   end
 end
--- app/models/genre.rb
+++
@@ -1,4 +0,0 @@
-class Genre < ActiveRecord::Base
-	validates :title, presence: true
-	has_many :books
-end
